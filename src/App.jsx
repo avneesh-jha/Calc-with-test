e7279e7d49973e4ec16ef59c33a9d32fa16df15a
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-04-30 00:55:53 +0530

    test-findBy--- for async request

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Calculator } from "./components/Calculator";
 import s from "./App.module.css";
+import { RandomUser } from "components/RandomUser/RandonUser";
 export function App() {
   return (
     <div className={s.root}>
       <Calculator defaultA={0} defaultB={0} defaultOperator={"+"} />
+      <RandomUser />
     </div>
   );
 }
